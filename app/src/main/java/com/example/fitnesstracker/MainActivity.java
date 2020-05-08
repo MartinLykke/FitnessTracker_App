bd40541be28cb1eab9723e24aa1d8af6d5d63b53
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2020-05-08 15:15:03 +0200

    RecycleView added

--- app/src/main/java/com/example/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.fitnesstracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<WorkoutsItems> workoutsList = new ArrayList<>();
+        workoutsList.add(new WorkoutsItems(R.drawable.bench_press, "Line1", "Line 2"));
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+       // mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new WorkoutsAdapter(workoutsList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/com/example/fitnesstracker/WorkoutsAdapter.java
@@ -0,0 +1,55 @@
+package com.example.fitnesstracker;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+
+public class WorkoutsAdapter extends RecyclerView.Adapter<WorkoutsAdapter.ExampleViewHolder> {
+    private ArrayList<WorkoutsItems> mWorkoutsList;
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+
+        public ExampleViewHolder(View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    public WorkoutsAdapter(ArrayList<WorkoutsItems> workoutsList) {
+        mWorkoutsList = workoutsList;
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.workouts_items, parent, false);
+        ExampleViewHolder evh = new ExampleViewHolder(v);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        WorkoutsItems currentItem = mWorkoutsList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWorkoutsList.size();
+    }
+}
